fcbced71981d984049bbcc6e18337e1925247d88
Luca Trautmann <guy_montag@posteo.net>
2023-08-19 11:45:46 +0100

    changes to maskify

--- src/masked.py
+++ src/masked.py
@@ -1,21 +1,24 @@
 # return masked string
-def maskify(cc):
-    '''
-    I need to take any input check the length of the input and convert any values except the last four into hashtags
-    '''
-    
+def maskifylegacy(cc):
+    """
+    need to take any input
+    check the length of the input
+    convert any values except the last four into hashtags
+    """
+
     target = str(cc)
     target_length = len(target)
-    l = [a for a in target]
-    
-    for i in range(len(l)-4):
-        l[i] = "#"
-            
-    l = tuple(l)
-    
-    output = "".join(l)
-    return(output)
+    target_list = [a for a in target]
+
+    for i in range(len(target_list) - 4):
+        target_list[i] = "#"
+
+    target_list_tup = tuple(target_list)
+
+    output = "".join(target_list_tup)
+    return output
+
 
 # return masked string IF STRING AS INPUT
 def maskify(cc):
-    return "#"*(len(cc)-4) + cc[-4:]
+    return "#" * (len(cc) - 4) + cc[-4:]
